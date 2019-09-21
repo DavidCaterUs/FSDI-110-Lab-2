fa095e49bfd78c1e016485cbcd578ae1a5fe066b
David <roberto@localhost.localdomain>
2019-09-21 15:35:59 -0300

    Lab 2

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CarRental.Models;
+
+namespace CarRental.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Console.WriteLine("The user wants the index view");
+            return View();
+
+        }
+
+        public IActionResult Hello(){
+          Console.WriteLine("The user wants the Hello view");
+          return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            Console.WriteLine("The user wants the Privacy View");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
